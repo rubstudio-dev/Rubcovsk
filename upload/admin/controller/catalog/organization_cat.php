ed2e8975bb6b6e15461c3630fcb9c60b958b1c20
Roman Kelesidis <roman25052006.kelesh@Gmail.com>
2023-06-24 22:28:19 +0700

    Updated

--- upload/admin/controller/catalog/organization_cat.php
+++ upload/admin/controller/catalog/organization_cat.php
@@ -32,11 +32,139 @@ class ControllerCatalogOrganizationCat extends Controller
 
 	protected function getList()
 	{
+		if (isset($this->request->get['sort'])) {
+			$sort = $this->request->get['sort'];
+		} else {
+			$sort = 'name';
+		}
+
+		if (isset($this->request->get['order'])) {
+			$order = $this->request->get['order'];
+		} else {
+			$order = 'ASC';
+		}
+
+		if (isset($this->request->get['page'])) {
+			$page = (int)$this->request->get['page'];
+		} else {
+			$page = 1;
+		}
+
+		$url = '';
+
+		if (isset($this->request->get['sort'])) {
+			$url .= '&sort=' . $this->request->get['sort'];
+		}
+
+		if (isset($this->request->get['order'])) {
+			$url .= '&order=' . $this->request->get['order'];
+		}
+
+		if (isset($this->request->get['page'])) {
+			$url .= '&page=' . $this->request->get['page'];
+		}
+
+		$data['breadcrumbs'] = array();
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
+		);
+
+		$data['breadcrumbs'][] = array(
+			'text' => 'Категории организаций',
+			'href' => $this->url->link('catalog/organization_cat', 'user_token=' . $this->session->data['user_token'] . $url, true)
+		);
+
+		$data['add'] = $this->url->link('catalog/organization_cat/add', 'user_token=' . $this->session->data['user_token'] . $url, true);
+		$data['delete'] = $this->url->link('catalog/organization_cat/delete', 'user_token=' . $this->session->data['user_token'] . $url, true);
+
+		$data['organizations'] = array();
+
+		$filter_data = array(
+			'sort' => $sort,
+			'order' => $order,
+			'start' => ($page - 1) * $this->config->get('config_limit_admin'),
+			'limit' => $this->config->get('config_limit_admin')
+		);
+
+		$organizations_cats_total = $this->model_catalog_organization_cat->getTotalOrganizationsCats();
+
+		$results = $this->model_catalog_organization_cat->getOrganizationsCats($filter_data);
+
+		foreach ($results as $result) {
+			$data['organizations'][] = array(
+				'id' => $result['id'],
+				'name' => $result['name'],
+				'alias' => $result['alias'],
+				'desc' => $result['description'],
+				'edit' => $this->url->link('catalog/organization_cat/edit', 'user_token=' . $this->session->data['user_token'] . '&id=' . $result['id'] . $url, true)
+			);
+		}
+
+		if (isset($this->error['warning'])) {
+			$data['error_warning'] = $this->error['warning'];
+		} else {
+			$data['error_warning'] = '';
+		}
+
+		if (isset($this->session->data['success'])) {
+			$data['success'] = $this->session->data['success'];
+
+			unset($this->session->data['success']);
+		} else {
+			$data['success'] = '';
+		}
+
+		if (isset($this->request->post['selected'])) {
+			$data['selected'] = (array)$this->request->post['selected'];
+		} else {
+			$data['selected'] = array();
+		}
+
+		$url = '';
+
+		if ($order == 'ASC') {
+			$url .= '&order=DESC';
+		} else {
+			$url .= '&order=ASC';
+		}
+
+		if (isset($this->request->get['page'])) {
+			$url .= '&page=' . $this->request->get['page'];
+		}
+
+		$data['sort_name'] = $this->url->link('catalog/organization_cat', 'user_token=' . $this->session->data['user_token'] . '&sort=name' . $url, true);
+		$data['sort_id'] = $this->url->link('catalog/organization_cat', 'user_token=' . $this->session->data['user_token'] . '&sort=id' . $url, true);
+
+		$url = '';
+
+		if (isset($this->request->get['sort'])) {
+			$url .= '&sort=' . $this->request->get['sort'];
+		}
+
+		if (isset($this->request->get['order'])) {
+			$url .= '&order=' . $this->request->get['order'];
+		}
+
+		$pagination = new Pagination();
+		$pagination->total = $organizations_cats_total;
+		$pagination->page = $page;
+		$pagination->limit = $this->config->get('config_limit_admin');
+		$pagination->url = $this->url->link('catalog/organization_cat', 'user_token=' . $this->session->data['user_token'] . $url . '&page={page}', true);
+
+		$data['pagination'] = $pagination->render();
+
+		$data['results'] = sprintf($this->language->get('text_pagination'), ($organizations_cats_total) ? (($page - 1) * $this->config->get('config_limit_admin')) + 1 : 0, ((($page - 1) * $this->config->get('config_limit_admin')) > ($organizations_cats_total - $this->config->get('config_limit_admin'))) ? $organizations_cats_total : ((($page - 1) * $this->config->get('config_limit_admin')) + $this->config->get('config_limit_admin')), $organizations_cats_total, ceil($organizations_cats_total / $this->config->get('config_limit_admin')));
+
+		$data['sort'] = $sort;
+		$data['order'] = $order;
+
 		$data['header'] = $this->load->controller('common/header');
 		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['footer'] = $this->load->controller('common/footer');
 
-		$this->response->setOutput($this->load->view('catalog/organization_cat', $data));
+		$this->response->setOutput($this->load->view('catalog/organization_cat_list', $data));
 	}
 
 	protected function getForm()
--- upload/admin/model/catalog/organization_cat.php
+++ upload/admin/model/catalog/organization_cat.php
@@ -25,13 +25,55 @@ class ModelCatalogOrganizationCat extends Model
 
 	public function getOrganizationsCats($data = array())
 	{
+		$sql = "SELECT * FROM " . DB_PREFIX . "org_categories";
+
+		$sort_data = array(
+			'name',
+			'id'
+		);
+
+		if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
+			$sql .= " ORDER BY " . $data['sort'];
+		} else {
+			$sql .= " ORDER BY id, name";
+		}
+
+		if (isset($data['order']) && ($data['order'] == 'DESC')) {
+			$sql .= " DESC";
+		} else {
+			$sql .= " ASC";
+		}
+
+		if (isset($data['start']) || isset($data['limit'])) {
+			if ($data['start'] < 0) {
+				$data['start'] = 0;
+			}
+
+			if ($data['limit'] < 1) {
+				$data['limit'] = 20;
+			}
+
+			$sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
+		}
+
+		$query = $this->db->query($sql);
+
+		return $query->rows;
 	}
 
 	public function getOrganizationCatDescriptions($organization_cat_id)
 	{
 	}
 
+	/**
+	 * Общее число категорий
+	 *
+	 * @return mixed
+	 */
 	public function getTotalOrganizationsCats()
 	{
+		$query = $this->db->query("SELECT COUNT(*) AS total FROM " . DB_PREFIX . "org_categories");
+
+		return $query->row['total'];
 	}
 }
