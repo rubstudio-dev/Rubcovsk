a33803b572c2e62a08ba65cc32d0b39bd92e90b1
Roman Kelesidis <roman25052006.kelesh@Gmail.com>
2023-07-06 01:00:51 +0700

    Дубль данных с header.php

--- upload/catalog/controller/common/home.php
+++ upload/catalog/controller/common/home.php
@@ -12,6 +12,25 @@ class ControllerCommonHome extends Controller
 			$this->document->addLink($this->config->get('config_url'), 'canonical');
 		}
 
+		// ==== [Начало] Дубль данных с header.php ====
+		// Адрес сайта
+		if ($this->request->server['HTTPS']) {
+			$server = $this->config->get('config_ssl');
+		} else {
+			$server = $this->config->get('config_url');
+		}
+
+		// Название сайта
+		$data['name'] = $this->config->get('config_name');
+
+		// Лого сайта
+		if (is_file(DIR_IMAGE . $this->config->get('config_logo'))) {
+			$data['logo'] = $server . 'image/' . $this->config->get('config_logo');
+		} else {
+			$data['logo'] = '';
+		}
+		// ==== [Конец] Дубль данных с header.php ====
+
 		/**
 		 * ========= Не используется =========
 		 *
