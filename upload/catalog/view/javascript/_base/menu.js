dc1bb6bdd021a63cdb2bd3518d62deb925af050d
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-07-10 23:26:38 +0700

    Updated

--- upload/catalog/view/javascript/_base/menu.js
+++ upload/catalog/view/javascript/_base/menu.js
@@ -1,1 +1,22 @@
-new Mmenu(document.querySelector("#menu"));
+new Mmenu(document.querySelector("#menu"), {
+    // Счётчик
+    "counters": {
+        "add": true
+    },
+    // Доп оверлеи
+    "navbars": [
+        {
+            "position": "top",
+            "content": [
+                "searchfield" // поиск
+            ]
+        },
+        {
+            "position": "bottom",
+            "content": [
+                // контент снизу страницы
+                "Сделано с любовью ❤"
+            ]
+        }
+    ]
+});
