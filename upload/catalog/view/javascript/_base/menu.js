cd817c0eb7804b24515081976a08302c6b6100db
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-07-14 00:26:47 +0700

    Updated

--- upload/catalog/view/javascript/_base/menu.js
+++ upload/catalog/view/javascript/_base/menu.js
@@ -1,39 +1,52 @@
-new Mmenu(
-    document.querySelector("#menu"),
-    {
-        theme: 'white',
-        // Счётчик
-        counters: {
-            add: true
-        },
-        // Поиск
-        searchfield: {
-            add: true,
-            placeholder: 'ул. Алтайская, дом 88',
-            splash: '<h5 class="text-muted text-center">Что ищем? 🙂</h5>',
-            title: 'Поиск',
-        },
-        // Навбары
-        navbars: [
+document.addEventListener(
+    "DOMContentLoaded", () => {
+        const menu = new Mmenu(
+            document.querySelector("#menu"),
             {
-                content: ['searchfield'],
-            },
-            {
-                content: ['prev', 'breadcrumbs', 'close'],
-            },
-            {
-                position: 'bottom',
-                content: [
-                    'Сделано с любовью ❤' // контент снизу страницы
+                theme: 'white',
+                // Счётчик
+                counters: {
+                    add: true
+                },
+                // Поиск
+                searchfield: {
+                    add: true,
+                    placeholder: 'ул. Алтайская, дом 88',
+                    splash: '<h5 class="text-muted text-center">Что ищем? 🙂</h5>',
+                    title: 'Поиск',
+                },
+                // Навбары
+                navbars: [
+                    {
+                        content: ['searchfield'],
+                    },
+                    {
+                        content: ['prev', 'breadcrumbs', 'close'],
+                    },
+                    {
+                        position: 'bottom',
+                        content: [
+                            'Сделано с любовью ❤' // контент снизу страницы
+                        ]
+                    }
                 ]
+            },
+            {
+                searchfield: {
+                    cancel: true,
+                    clear: true,
+                },
+                language: 'ru' // локаль
             }
-        ]
-    },
-    {
-        searchfield: {
-            cancel: true,
-            clear: true,
-        },
-        language: 'ru' // локаль
+        );
+
+        const api = menu.API;
+
+        document.querySelector("#menu_open_btn")
+            .addEventListener(
+                "click", () => {
+                    api.open();
+                }
+            );
     }
 );
